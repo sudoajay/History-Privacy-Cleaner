02a8feced76272c159a2f033bb26fc5bbd98233a
sudoajay <sudoajay@gmail.com>
2020-10-24 21:32:47 +0530

    DOne

---
+++ app/src/main/java/com/sudoajay/historycachecleaner/activity/progress/ProgressActivity.kt
@@ -0,0 +1,12 @@
+package com.sudoajay.historycachecleaner.activity.progress
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sudoajay.historyprivacycleaner.R
+
+class ProgressActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_progress)
+    }
+}
