5164488aa457997d8a7c729d3a05e799a957089a
sudoajay <sudoajay@gmail.com>
2020-10-30 21:42:54 +0530

    Added splash screen

---
+++ app/src/main/java/com/sudoajay/historycachecleaner/activity/splashScreen/Splash.kt
@@ -0,0 +1,54 @@
+package com.sudoajay.historycachecleaner.activity.splashScreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
+import com.sudoajay.historycachecleaner.activity.BaseActivity
+import com.sudoajay.historycachecleaner.activity.main.MainActivity
+import com.sudoajay.historyprivacycleaner.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+
+class Splash : BaseActivity() {
+    private var isFirstAnimation = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        /*Simple hold animation to hold ImageView in the centre of the screen at a slightly larger
+        scale than the ImageView's original one.*/
+        val hold = AnimationUtils.loadAnimation(this, R.anim.hold)
+
+        /* Translates ImageView from current position to its original position, scales it from
+        larger scale to its original scale.*/
+        val translateScale = AnimationUtils.loadAnimation(this, R.anim.translate_scale)
+        val imageView =
+            header_icon
+        translateScale.setAnimationListener(object : AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+            override fun onAnimationEnd(animation: Animation) {
+                if (!isFirstAnimation) {
+                    imageView.clearAnimation()
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                isFirstAnimation = true
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+        })
+        hold.setAnimationListener(object : AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+            override fun onAnimationEnd(animation: Animation) {
+                imageView.clearAnimation()
+                imageView.startAnimation(translateScale)
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+        })
+        imageView.startAnimation(hold)
+    }
+}
