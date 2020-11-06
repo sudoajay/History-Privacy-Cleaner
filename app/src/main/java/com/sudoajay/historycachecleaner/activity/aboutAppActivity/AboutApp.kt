fc9b509ef3a18ed92af76c727f239c69adb8951c
sudoajay <sudoajay@gmail.com>
2020-11-06 09:22:39 +0530

    added appUpdateManager

--- app/src/main/java/com/sudoajay/historycachecleaner/activity/aboutAppActivity/AboutApp.kt
+++ app/src/main/java/com/sudoajay/historycachecleaner/activity/aboutAppActivity/AboutApp.kt
@@ -1,21 +1,47 @@
 package com.sudoajay.historycachecleaner.activity.aboutAppActivity
 
+import android.app.Activity
 import android.content.Intent
+import android.content.IntentSender
 import android.graphics.Color
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.sudoajay.historycachecleaner.activity.BaseActivity
+import com.sudoajay.historycachecleaner.helper.CustomToast
 import com.sudoajay.historyprivacycleaner.BuildConfig
 import com.sudoajay.historyprivacycleaner.R
 import com.sudoajay.historyprivacycleaner.databinding.ActivityAboutAppBinding
 
 class AboutApp : BaseActivity() {
+    private val TAG= "AboutAppTAG"
+    private val appUpdateManager: AppUpdateManager by lazy { AppUpdateManagerFactory.create(this) }
+    private val appUpdatedListener: InstallStateUpdatedListener by lazy {
+        object : InstallStateUpdatedListener {
+            override fun onStateUpdate(installState: InstallState) {
+                when {
+                    installState.installStatus() == InstallStatus.DOWNLOADED -> popupCompleteToast()
+                    installState.installStatus() == InstallStatus.INSTALLED -> appUpdateManager.unregisterListener(
+                        this
+                    )
+                    else -> Log.d(TAG, installState.installStatus().toString())
+                }
+            }
+        }
+    }
     private lateinit var binding: ActivityAboutAppBinding
     private var isDarkTheme: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +49,7 @@ class AboutApp : BaseActivity() {
         super.onCreate(savedInstanceState)
         isDarkTheme = isDarkMode(applicationContext)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!isDarkTheme )
+            if (!isDarkTheme)
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) window.setDecorFitsSystemWindows(
                     false
                 ) else window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
@@ -32,6 +58,7 @@ class AboutApp : BaseActivity() {
         binding.activity = this
         changeStatusBarColor()
     }
+
 
     override fun onResume() {
         super.onResume()
@@ -57,12 +84,86 @@ class AboutApp : BaseActivity() {
             }
             binding.toolbar.navigationIcon = it
         }
+
+
+        appUpdateManager
+            .appUpdateInfo
+            .addOnSuccessListener { appUpdateInfo ->
+
+                // If the update is downloaded but not installed,
+                // notify the user to complete the update.
+                if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                    popupCompleteToast()
+                }
+
+                //Check if Immediate update is required
+                try {
+                    if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                        // If an in-app update is already running, resume the update.
+                        appUpdateManager.startUpdateFlowForResult(
+                            appUpdateInfo,
+                            AppUpdateType.IMMEDIATE,
+                            this,
+                            APP_UPDATE_REQUEST_CODE
+                        )
+                    }
+                } catch (e: IntentSender.SendIntentException) {
+                    e.printStackTrace()
+                }
+            }
+
+        checkForAppUpdate()
     }
 
-    fun getVersionCode():String{
+    private fun checkForAppUpdate() {
+        // Returns an intent object that you use to check for an update.
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                // Request the update.
+                try {
+                    val installType = when {
+                        appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE) -> AppUpdateType.FLEXIBLE
+                        appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE) -> AppUpdateType.IMMEDIATE
+                        else -> null
+                    }
+                    if (installType == AppUpdateType.FLEXIBLE) appUpdateManager.registerListener(
+                        appUpdatedListener
+                    )
+
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        installType!!,
+                        this,
+                        APP_UPDATE_REQUEST_CODE
+                    )
+                } catch (e: IntentSender.SendIntentException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == APP_UPDATE_REQUEST_CODE) {
+            if (resultCode != Activity.RESULT_OK)
+                CustomToast.toastIt(applicationContext,getString(R.string.update_failed_text))
+
+        }
+    }
+
+
+    private fun popupCompleteToast() {
+       CustomToast.toastIt(applicationContext, getString(R.string.apk_available_text))
+    }
+
+    fun getVersionCode(): String {
         val versionName = BuildConfig.VERSION_NAME
 
-        return getString(R.string.version_info_text,versionName)
+        return getString(R.string.version_info_text, versionName)
     }
 
     fun openPrivacyPolicy() {
@@ -91,5 +192,9 @@ class AboutApp : BaseActivity() {
                 window.statusBarColor = Color.TRANSPARENT
             }
         }
+    }
+
+    companion object {
+        private const val APP_UPDATE_REQUEST_CODE = 1991
     }
 }
